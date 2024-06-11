d23bd92f8ee485d0ba07ecc2e2060c9f87760495
nizar095 <nizaranantayuda@gmail.com>
2024-06-11 21:57:56 +0700

    Tampilang Loading Fixes #5

---
+++ lib/authenticationScreen/loading_screen.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushNamed(context, '/login');
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+         backgroundColor: const Color(0xFF333333),
+        body: Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 414,
+            height: 896,
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(color: Color.fromRGBO(51, 51, 51, 1)),
+            child: Stack(
+              children: [
+                const Center(
+                  child: Positioned(
+                    child: SpinKitRing(
+                      color:Color.fromRGBO(44, 61, 108, 1),
+                      size: 80,
+          
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Positioned(
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("images/LogoTangan.png"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}

aa7988390dbf9020b5cea0d5b1ee893b767edceb
Memmodz <rihdoriansahputra56@gmail.com>
2024-06-19 04:19:45 +0700

    Tampilan SignUp Fixes #13

--- lib/view/signup/sign_up_screen.dart
+++ lib/view/signup/sign_up_screen.dart
@@ -1,8 +1,12 @@
-import 'package:dattingapp/utils/utils.dart';
-import 'package:dattingapp/view_model/signup/signup_controller.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
+
 import 'package:dattingapp/res/component/input_text_field.dart';
 import 'package:dattingapp/res/component/round_botoon.dart';
+import 'package:dattingapp/utils/utils.dart';
+import 'package:dattingapp/view/login/login_screen.dart';
+import 'package:dattingapp/view_model/signup/signup_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -15,9 +19,9 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   final _formKey = GlobalKey<FormState>();
 
-  final emailControllor = TextEditingController();
-  final passwordControllor = TextEditingController();
-  final usernameControllor = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final usernameController = TextEditingController();
 
   final emailFocusNode = FocusNode();
   final passwordFocusNode = FocusNode();
@@ -26,9 +30,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   void dispose() {
     super.dispose();
-    emailControllor.dispose();
-    passwordControllor.dispose();
-    usernameControllor.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    usernameController.dispose();
     emailFocusNode.dispose();
     passwordFocusNode.dispose();
     usernameFocusNode.dispose();
@@ -40,135 +44,153 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsetsDirectional.symmetric(
-              horizontal: 20, vertical: 10),
-          child: ChangeNotifierProvider(
-            create: (_) => SignUpController(),
-          child: Consumer<SignUpController>(
-            builder: (context, provider, child){
-              return SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: height * .15,
-                ),
-                Text(
-                  'Register to App',
-                  style: Theme.of(context).textTheme.displaySmall,
-                ),
-                SizedBox(
-                  height: height * .01,
-                ),Text(
-                  'Enter your username,email address and password\nto Register your account',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                  textAlign: TextAlign.center,
-                ),
-    
-                SizedBox(
-                  height: height * .01,
-                ),
-                Form(
-                  key: _formKey,
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                        top: height * .06, bottom: height * 0.01),
-                    child: Column(
-                      children: [
-                        InputTextField(
-                            enable: true,
-                            myController: usernameControllor,
-                            focusNode: usernameFocusNode,
-                            onFieldSubmittedValue: (value) {},
-                            keyboardType: TextInputType.emailAddress,
-                            isPasswordField: false,
-                            hint: 'Username',
-                            onValidator: (value) {
-                              return value.isEmpty ? 'Enter Username' : null;
-                            }),
-                        SizedBox(
-                          height: height * 0.01,
+            padding: const EdgeInsetsDirectional.symmetric(
+                horizontal: 20, vertical: 10),
+            child: ChangeNotifierProvider(
+              create: (_) => SignUpController(),
+              child: Consumer<SignUpController>(
+                  builder: (context, provider, child) {
+                return SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: height * .15,
+                      ),
+                      Text(
+                        'Create an Account',
+                        style: Theme.of(context).textTheme.displaySmall,
+                      ),
+                      SizedBox(
+                        height: height * .01,
+                      ),
+                      Text(
+                        'Enter your username,email address and password\nto Create your account',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: height * .01,
+                      ),
+                      Form(
+                        key: _formKey,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              top: height * .06, bottom: height * 0.01),
+                          child: Column(
+                            children: [
+                              InputTextField(
+                                  enable: true,
+                                  myController: usernameController,
+                                  focusNode: usernameFocusNode,
+                                  onFieldSubmittedValue: (value) {},
+                                  keyboardType: TextInputType.emailAddress,
+                                  isPasswordField: false,
+                                  hint: 'Username',
+                                  onValidator: (value) {
+                                    return value.isEmpty
+                                        ? 'Enter Username'
+                                        : null;
+                                  }),
+                              SizedBox(
+                                height: height * 0.01,
+                              ),
+                              InputTextField(
+                                  enable: true,
+                                  myController: emailController,
+                                  focusNode: emailFocusNode,
+                                  onFieldSubmittedValue: (value) {
+                                    Utils.fieldFocus(context, emailFocusNode,
+                                        passwordFocusNode);
+                                  },
+                                  keyboardType: TextInputType.emailAddress,
+                                  isPasswordField: false,
+                                  hint: 'Email',
+                                  onValidator: (value) {
+                                    return value.isEmpty ? 'Enter Email' : null;
+                                  }),
+                              SizedBox(
+                                height: height * 0.01,
+                              ),
+                              InputTextField(
+                                  enable: true,
+                                  myController: passwordController,
+                                  focusNode: passwordFocusNode,
+                                  onFieldSubmittedValue: (value) {},
+                                  keyboardType: TextInputType.visiblePassword,
+                                  isPasswordField: true,
+                                  hint: 'Password',
+                                  onValidator: (value) {
+                                    return value.isEmpty
+                                        ? 'Enter Password'
+                                        : null;
+                                  })
+                            ],
+                          ),
                         ),
-                        InputTextField(
-                            enable: true,
-                            myController: emailControllor,
-                            focusNode: emailFocusNode,
-                            onFieldSubmittedValue: (value) {
-                              Utils.fieldFocus(context, emailFocusNode, passwordFocusNode);
-                            },
-                            keyboardType: TextInputType.emailAddress,
-                            isPasswordField: false,
-                            hint: 'Email',
-                            onValidator: (value) {
-                              return value.isEmpty ? 'Enter Email' : null;
-                            }),
-                        SizedBox(
-                          height: height * 0.01,
-                        ),
-                        InputTextField(
-                            enable: true,
-                            myController: passwordControllor,
-                            focusNode: passwordFocusNode,
-                            onFieldSubmittedValue: (value) {},
-                            keyboardType: TextInputType.visiblePassword,
-                            isPasswordField: true,
-                            hint: 'Password',
-                            onValidator: (value) {
-                              return value.isEmpty ? 'Enter Password' : null;
-                            })
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                RoundBotoon(
-                  title: 'Sign Up',
-                  loading: provider.loading,
-                  onPressed: () {
-                    
-                    if(_formKey.currentState!.validate ()){
-                      provider.signUp(context, usernameControllor.text, emailControllor.text, passwordControllor.text);
-                      
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: height * 0.03,
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/login');
-                  },
-                  child: Text.rich(
-                    TextSpan(
-                      text: "Already have an account? ",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodySmall!
-                          .copyWith(fontSize: 14),
-                      children: [
-                        TextSpan(
-                            text: 'LogIn',
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      RoundBotoon(
+                        title: 'Sign Up',
+                        loading: provider.loading,
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            provider.signUp(context, usernameController.text,
+                                emailController.text, passwordController.text);
+                          } else {
+                            Timer(
+                              Duration(seconds: 1),
+                              () => Navigator.of(context).push(
+                                PageTransition(
+                                  child: SignUpScreen(),
+                                  type: PageTransitionType.fade,
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: height * 0.03,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            PageTransition(
+                              child: LoginScreen(),
+                              type: PageTransitionType.fade,
+                            ),
+                          );
+                        },
+                        child: Text.rich(
+                          TextSpan(
+                            text: "Already have an account? ",
                             style: Theme.of(context)
                                 .textTheme
-                                .bodyLarge!
-                                .copyWith(
-                                    fontSize: 14,
-                                    decoration: TextDecoration.underline)),
-                      ],
-                    ),
+                                .bodySmall!
+                                .copyWith(fontSize: 14),
+                            children: [
+                              TextSpan(
+                                  text: 'LogIn',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge!
+                                      .copyWith(
+                                          fontSize: 14,
+                                          decoration:
+                                              TextDecoration.underline)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
-          );
-          }
-          ),
-          )
-        ),
+                );
+              }),
+            )),
       ),
     );
   }
--- lib/view_model/signup/signup_controller.dart
+++ lib/view_model/signup/signup_controller.dart
@@ -1,60 +1,55 @@
-import 'package:dattingapp/utils/utils.dart';
+
+import 'package:dattingapp/utils/routes/route_name.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
+import '../../utils/utils.dart';
+import '../services/session_manager.dart';
 
 class SignUpController with ChangeNotifier {
-DatabaseReference ref = FirebaseDatabase.instance.ref().child("User");
+  FirebaseAuth auth = FirebaseAuth.instance;
+  final DatabaseReference dbref = FirebaseDatabase.instance.ref().child("User");
 
- FirebaseAuth auth = FirebaseAuth.instance;
- bool _loading = false;
-bool get loading => _loading;
+  bool _loading = false;
+  bool get loading => _loading;
 
+  setLoading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
 
- setLoading(bool value){
-  _loading = value; 
-  notifyListeners();
- }
- 
-
-void signUp(BuildContext context, username, String email, String password){
-  setLoading(true);
-  try{ 
-    auth.createUserWithEmailAndPassword(
-      email: email, 
-      password: password
-      ).then((value){
-
-        ref.child(value.user!.uid.toString()).set({
-          'uid': value.user!.uid.toString(),
-          'email': value.user!.email.toString(),
-          'onlineStatus':'noOne',
-          'phone': '' ,
-          'userName': username,
-          'profile': '' 
-        }).then((value){
-          
+  void signUp(
+      BuildContext context, username, String email, String password) async {
+    setLoading(true);
+    try {
+      await auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((value) {
+            SessionController().userId = value.user!.uid.toString();
+        dbref.child(value.user!.uid.toString()).set({
+          'Uid': value.user!.uid.toString(),
+          'Email': value.user!.email.toString(),
+          'OnlineStatus': false,
+          'Phone': '',
+          'UserName': username,
+          'Profile': ''
+        }).then((value) {
           setLoading(false);
-        }).onError((error, stackTrace){
+        }).onError((error, stackTrace) {
           setLoading(false);
           Utils.toasstMessage(error.toString());
         });
-        
+
         setLoading(false);
-        Navigator.pushNamed(context, '/profil');
-      }).onError((error, stackTrace){
+        Navigator.pushNamed(context, RouteName.loginScreen);
+      }).onError((error, stackTrace) {
         setLoading(false);
         Utils.toasstMessage(error.toString());
       });
-
-
-  }catch(e){
-    setLoading(false);
-    Utils.toasstMessage(e.toString());
+    } catch (e) {
+      setLoading(false);
+      Utils.toasstMessage(e.toString());
+    }
   }
-
-}
-
-
 }

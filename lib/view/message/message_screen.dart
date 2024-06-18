8509bcaa533d66b2144721a307c98648241d1f7c
mesneezy <anggakiemas2521@gmail.com>
2024-06-19 05:45:20 +0700

    Tampilan Message Fixes #18

--- lib/view/message/message_screen.dart
+++ lib/view/message/message_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dattingapp/view/profile/profile_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class MessageScreen extends StatelessWidget {
   final List<Message> messages = [
@@ -40,48 +42,101 @@ class MessageScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFF333333),
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(35.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                
+        child: Container(
+          margin: EdgeInsets.only(top: 85),
+          child: Column(
+            children: <Widget>[
+              // Top icons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 55,
+                    height: 55,
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF333333),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: IconButton(
+                      icon: Icon(Icons.person_outline_rounded),
+                      iconSize: 35,
+                      color: Colors.white,
+                      onPressed: () => Navigator.of(context).push(
+                        PageTransition(
+                          child: ProfileScreen(),
+                          type: PageTransitionType.fade,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 65),
+                  Container(
+                    width: 55,
+                    height: 55,
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF333333),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: IconButton(
+                      icon: Image.asset('images/flame.png'),
+                      iconSize: 35,
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ),
+                  SizedBox(width: 65),
+                  Container(
+                    width: 55,
+                    height: 55,
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(44, 61, 108, 1),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: IconButton(
+                      icon: Icon(Icons.messenger_outline_rounded),
+                      iconSize: 35,
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: messages.length,
-                itemBuilder: (context, index) {
-                  final message = messages[index];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage(message.imageUrl),
-                    ),
-                    title: Text(
-                      message.name,
-                      style: TextStyle(
-                        color: message.isHighlighted ? Colors.pink : Colors.white,
-                        fontWeight: message.isHighlighted ? FontWeight.bold : FontWeight.normal,
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    final message = messages[index];
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage(message.imageUrl),
                       ),
-                    ),
-                    subtitle: Text(
-                      message.message,
-                      style: TextStyle(
-                        color: Colors.white,
+                      title: Text(
+                        message.name,
+                        style: TextStyle(
+                          color: message.isHighlighted
+                              ? Colors.pink
+                              : Colors.white,
+                          fontWeight: message.isHighlighted
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                        ),
                       ),
-                    ),
-                    trailing: Text(
-                      message.time,
-                      style: TextStyle(
-                        color: Colors.grey,
+                      subtitle: Text(
+                        message.message,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  );
-                },
+                      trailing: Text(
+                        message.time,
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
